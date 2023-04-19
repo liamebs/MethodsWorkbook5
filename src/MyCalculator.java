7336a9889b81aaeeddb961ea51cc279567e7aa99
liamebs <simon.ebsworth@yahoo.co.uk>
2023-04-19 16:12:40 +0100

    General Update

--- src/MyCalculator.java
+++ src/MyCalculator.java
@@ -6,38 +6,37 @@ public class MyCalculator {
         double num2 = askNum();
 
         // call respective methods to perform calculations
-        addNum(num1, num2);
-        subtractNum(num1, num2);
-        multiplyNum(num1, num2);
-        divideNum(num1, num2);
-
-    }
-
-    public static void addNum(double num1, double num2) {
-        double add = num1 + num2;
+        double add = addNum(num1, num2);
         System.out.println("\n" + num1 + " + " + num2 + " = " + add);
 
-    }
-
-    public static void subtractNum(double num1, double num2) {
-        double subtract = num1 - num2;
+        double subtract = subtractNum(num1, num2);
         System.out.println("\n" + num1 + " - " + num2 + " = " + subtract);
 
-    }
-
-    public static void multiplyNum(double num1, double num2) {
-        double multiply = num1 * num2;
+        double multiply = multiplyNum(num1, num2);
         System.out.println("\n" + num1 + " * " + num2 + " = " + multiply);
 
-    }
-
-    public static void divideNum(double num1, double num2) {
-        double divide = num1 / num2;
+        double divide = divideNum(num1, num2);
         System.out.println("\n" + num1 + " / " + num2 + " = " + divide);
 
     }
 
-    public static double askNum() { // initialise askInt() with an double return value
+    public static double addNum(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtractNum(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiplyNum(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double divideNum(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public static double askNum() { // initialise askNum() with a double return value
         Scanner scanner = new Scanner(System.in); // instantiate Scanner object "scanner"
         System.out.print("Enter a number: "); // request double
         while (!scanner.hasNextDouble()) { // create while loop to monitor input validity
